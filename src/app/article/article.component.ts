e0c0d0d0c3a7fecb7005c62140fdf35ce3f61912
MichealFallon211436 <s00211436@atu.ie>
2023-02-21 16:24:27 +0000

    Video 3 Finished

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent {
+  votes:number;
+  title:string;
+  link:string;
+  constructor() {
+    this.title="Angular";
+    this.link="https://angular.io";
+    this.votes=10;
+  }
+  voteUp():boolean {
+    this.votes ++;
+    return false;
+  }
+  voteDown():boolean {
+    this.votes --;
+    return false;
+  }
+
+}
