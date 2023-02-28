0130c71a735efb038514a2cef8eb96d71622f147
MichealFallon211436 <s00211436@atu.ie>
2023-02-28 16:11:35 +0000

    mostly finished video 5

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,12 +8,13 @@ import { Article } from './article.model';
 })
 export class ArticleComponent {
 
-  article:Article;
+  @Input()
+  article!: Article;
 
   
   constructor() {
    
-    this.article =  new Article("Angular","https://angular.io",10)
+    
     
   }
   voteUp():boolean {
